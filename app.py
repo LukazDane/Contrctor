4b2465383dc0d5b0c9119b248d2fa9d1929b1efe
Luke <deandrevidal@aol.com>
2019-10-06 11:26:57 -0700

    added mongodb shop model

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Pokemart
+items = db.items
 
 app = Flask(__name__)
 
@@ -10,20 +15,20 @@ app = Flask(__name__)
 
 
 # mock items - delete later ₽
-items = [
-    {'title': 'Pokeball',
-        'description': 'A standard pokeball with a 50% success rate!', 'price': '200'},
-    {'title': 'Great Ball',
-        'description': 'An upgraded pokeball with a 65% success rate!', 'price': '400'},
-    {'title': 'Ultra Ball',
-        'description': 'The highest grade of pokeball with a 80% success rate!', 'price': '800'}
-]
+# items = [
+#     {'title': 'Pokeball',
+#         'description': 'A standard pokeball with a 50% success rate!', 'price': '200'},
+#     {'title': 'Great Ball',
+#         'description': 'An upgraded pokeball with a 65% success rate!', 'price': '400'},
+#     {'title': 'Ultra Ball',
+#         'description': 'The highest grade of pokeball with a 80% success rate!', 'price': '800'}
+# ]
 
 
 @app.route('/')
 def items_index():
     """Display all itms"""
-    return render_template("items_index.html", items=items)
+    return render_template("items_index.html", items=items.find())
 
 
 if __name__ == '__main__':
